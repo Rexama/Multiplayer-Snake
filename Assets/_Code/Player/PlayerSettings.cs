477ca534779294081f5ce5d427e7cf4626b1d101
Rekin Barışıcı <rekinbar2000@gmail.com>
2023-07-03 15:19:15 +0300

    movement and grid system

---
+++ Assets/_Code/Player/PlayerSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _Code.Player
+{
+    [CreateAssetMenu(fileName = "PlayerSettings", menuName = "Settings", order = 0)]
+    public class PlayerSettings : ScriptableObject
+    {
+        public float stepTime;
+        public float offset;
+    }
+}
