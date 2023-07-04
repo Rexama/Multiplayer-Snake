203247544190d08d44c0d4bae09ebd8c4e892e00
Rekin Barışıcı <rekinbar2000@gmail.com>
2023-07-04 21:52:16 +0300

    food spawner?

---
+++ Assets/_Code/GridBase/CellInfo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace _Code.Grid
+{
+    public class CellInfo
+    {
+        public Vector2 Position;
+        public CellType CellType;
+        
+        public CellInfo(Vector2 position, CellType cellType)
+        {
+            this.Position = position;
+            this.CellType = cellType;
+        }
+    }
+    public enum CellType
+    {
+        Empty,
+        Wall,
+        Player1,
+        Player2,
+        Food,
+    }
+}
